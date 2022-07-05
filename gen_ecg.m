736d34036412bd9f85a0a3afff5efb3f6ba51bd0
Steve Farra <st.farra@gmail.com>
2022-07-05 08:31:19 -0700

    changed comments

--- gen_ecg.m
+++ gen_ecg.m
@@ -1,17 +1,17 @@
-# Required by ecgsyn.m to run
+% Required by ecgsyn.m to run
 pkg load signal
 
-# Parameters
+% Parameters
 SAMPLING_FREQ_HZ = 256;
 NUM_BEATS = 10;
 FILENAME = 'ecg.txt';
 
 ecg_mv = ecgsyn(SAMPLING_FREQ_HZ, NUM_BEATS);
 
-# Output of ecgsyn() is in millivolts so convert to volts
+% Output of ecgsyn() is in millivolts so convert to volts
 ecg = ecg_mv * 1e-3;
 
-# Write to file compatible with LTspice
+% Write to file compatible with LTspice
 file_id = fopen(FILENAME,'w');
 for k = 1: length(ecg)
    fprintf(file_id, '%6.6f,%6.6f\n' , (k-1)/SAMPLING_FREQ_HZ, ecg(k));
